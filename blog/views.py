0398bb2a81ad82b1ff3c607049d8413cea74335f
RazorOchka <yakopl@hotmail.com>
2021-12-24 14:34:50 +0300

    CRUD added

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.db.models import fields
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 class BlogListView(ListView):
@@ -11,3 +13,18 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.htm'
+    
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.htm'
+    fields = '__all__'
+    
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.htm'
+    fields = ['title', 'body']
+    
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.htm'
+    success_url = reverse_lazy('home')
